f0894f5fd1b6c7366fc5a62f266183fc780c3aaa
Angelica Hernandez <gel.lorraine@gmail.com>
2021-12-11 00:59:32 -0800

    Annimation to project Cards

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,49 +1,55 @@
 import React from "react";
 import { Card, Button, Image, Row, Col, Container } from "react-bootstrap";
 import "../App.css";
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
 
 function ProjectCard(props) {
   return (
     <Card className="">
       <Row className={props.id}>
         <Col className="projectCard" sm={6}>
-          <Image className="image" src={props.src} />
+          <Slide left>
+            <Image className="image" src={props.src} />{" "}
+          </Slide>
         </Col>
         <Col className="projectCard" sm={6}>
           <Card.Body className="m-2">
-            <Card.Text>
-              <h3
-                style={{
-                  textAlign: "center",
-                  fontFamily: "Bebas Neue",
-                  fontSize: "38px",
-                }}
-              >
-                {" "}
-                {props.title}{" "}
-              </h3>
-              <p style={{ fontSize: "18px" }}> {props.text}</p>
-            </Card.Text>
+            <Fade bottom>
+              <Card.Text>
+                <h3
+                  style={{
+                    textAlign: "center",
+                    fontFamily: "Bebas Neue",
+                    fontSize: "38px",
+                  }}
+                >
+                  {" "}
+                  {props.title}{" "}
+                </h3>
+                <p style={{ fontSize: "18px" }}> {props.text}</p>
+              </Card.Text>
 
-            <Container style={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                size="lg"
-                className="m-1 btnGrad"
-                target="_blank"
-                href={`${props.github}`}
-              >
-                Repository
-              </Button>
+              <Container style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  size="lg"
+                  className="m-1 btnGrad"
+                  target="_blank"
+                  href={`${props.github}`}
+                >
+                  Repository
+                </Button>
 
-              <Button
-                size="lg"
-                className="m-1 btnGrad"
-                target="_blank"
-                href={`${props.launch}/`}
-              >
-                Launch App
-              </Button>
-            </Container>
+                <Button
+                  size="lg"
+                  className="m-1 btnGrad"
+                  target="_blank"
+                  href={`${props.launch}/`}
+                >
+                  Launch App
+                </Button>
+              </Container>
+            </Fade>
           </Card.Body>
         </Col>
       </Row>
