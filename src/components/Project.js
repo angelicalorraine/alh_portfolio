f112002a9218abc4c8b58ce9c3785b809c996859
Angelica Hernandez <gel.lorraine@gmail.com>
2021-12-10 23:03:04 -0800

    Project image update | Style updates

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import "../App.css";
 import ProjectCard from "./projectCard";
-import study from "../assets/SBM.png";
-import favor from "../assets/FTM.png";
-import city from "../assets/HomeC.png";
+import study from "../assets/SB.png";
+import favor from "../assets/FT.png";
+import city from "../assets/HC.png";
 
 const projects = [
   {
@@ -15,26 +15,26 @@ const projects = [
     github: "https://github.com/angelicalorraine/Home_City",
     launch: "https://home-city-final.herokuapp.com",
 
-    id: "SB",
+    id: "RGT",
   },
   {
-    name: "rgtProject",
+    name: "lftProject",
     src: study,
     title: "Studdy Buddy",
     text: "Our knowledge and newly learned skills were put to the test by creating a web application from scratch.We conceived and executed a design and integrated data received from multiple server- side API requests. Study Buddy was built with the developer in mind, we are committed to being lifelong students.When searching a question or topic and the results will show you only resolved results from StackOverflow along with suggested tutorials! ",
     github: "https://github.com/angelicalorraine/Study_Buddy",
     launch: "https://angelicalorraine.github.io/Study_Buddy/",
-    id: "RGT",
+    id: "LFT",
   },
 
   {
-    name: "lftProject",
+    name: "rgtProject",
     src: favor,
     title: "Favor Trader",
     text: "Coming across an important milestone in our bootcamp journey we designed and built our first  full stack web application. We executed a design using the MVC paradigm, we combined a robust back end with servers, databases, advanced APIs, and user authentication with an intuitive front end. Favor Trader is a neighborhood bartering platform for favors.It contains an Activity Feed where all user’s favors needs are listed.To begin trading, each user will need to log in or sign up and create a user profile.",
     github: "https://github.com/angelicalorraine/Favor-Trader",
     launch: "https://favortrader.herokuapp.com",
-    id: "SB",
+    id: "RGT",
   },
 ];
 
--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,26 +1,42 @@
 import React from "react";
-import { Card, Button, Image, Row, Col } from "react-bootstrap";
+import { Card, Button, Image, Row, Col, Container } from "react-bootstrap";
 import "../App.css";
 
 function ProjectCard(props) {
   return (
     <Card className="">
       <Row className={props.id}>
-        <Col className="borderCard" sm={5}>
+        <Col className="projectCard" sm={6}>
           <Image className="image" src={props.src} height="250" />
         </Col>
-        <Col sm={6}>
-          <Card.Body>
+        <Col className="projectCard" sm={6}>
+          <Card.Body className="m-3">
             <Card.Text>
               <h3> {props.title} </h3>
               <p style={{ fontSize: "18px" }}> {props.text}</p>
             </Card.Text>
-            <Button variant="primary" target="_blank" href={`${props.github}`}>
-              Repo
-            </Button>
-            <Button variant="primary" target="_blank" href={`${props.launch}/`}>
-              Launch
-            </Button>
+
+            <Container style={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                size="lg"
+                className="m-1"
+                variant="primary"
+                target="_blank"
+                href={`${props.github}`}
+              >
+                Repo
+              </Button>
+
+              <Button
+                size="lg"
+                className="m-1"
+                variant="primary"
+                target="_blank"
+                href={`${props.launch}/`}
+              >
+                Launch
+              </Button>
+            </Container>
           </Card.Body>
         </Col>
       </Row>
