070b88001143ff2519549f4b933de41f9c0e4094
Angelica Hernandez <gel.lorraine@gmail.com>
2021-12-23 00:15:21 -0800

    about me updated

--- src/components/About.js
+++ src/components/About.js
@@ -19,16 +19,14 @@ const About = () => (
             <p className="aboutme" style={{ textAlign: "center" }}>
               I'm am a certified UX/UI Designer & Full Stack Developer based in
               Orange County, I received my certificates through UC Irvine
-              Division of Continuing Education. Computers and technnology have
-              always come natural to me, whether it was setting things up,
-              fixing issues, learning something new or just lending a helping
-              hand. I always knew I wanted to be apart of this industry in one
-              way or another, but now after completing The Coding Bootcamp I
-              feel like I finally found my passion in software development.
-            </p>{" "}
+              Division of Continuing Education. Technology has always come
+              natural to me, whether it was setting things up, fixing issues,
+              learning something new or just lending a helping hand. My dream
+              has always been to pursue a career in this industry, and I can
+              confidently say I found my passion in software development.
+            </p>
             <h5 style={{ fontStyle: "italic" }}>
-              {" "}
-              Let's create something special{" "}
+              Let's create something special
             </h5>
           </Fade>
         </Col>
